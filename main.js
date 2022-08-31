67f7b9376c65318656ae70478dfc5125ac194f8c
shoyeb-13 <shoyebbhuiyan13@gmail.com>
2022-09-01 01:20:21 +0600

    done

---
+++ main.js
@@ -0,0 +1,8 @@
+ document.getElementById('yes-btn').addEventListener('click', function(){
+        document.getElementById('text').innerText = "Ready to marry me💟";
+        text.style.color = 'deeppink';
+        })
+        document.getElementById('of-course').addEventListener('click', function(){
+        document.getElementById('text').innerText = "Love you too babu 💝   ";
+        text.style.color = 'fuchsia';
+        })
